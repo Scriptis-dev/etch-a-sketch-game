c0a356e2325acda3f548c766f908bf813b18a592
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-10-18 00:42:56 +0200

    createGrid function Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,22 @@
 const clearBtn = document.querySelector('#clear-');
 // const gameContainerWindow = document.querySelector('#grid-container');
 
-function createGrid(size = 32 * 44, cssClass = 'medium-grid') {
-  const gameContainerWindow = document.querySelector('#grid-container');
-
-  gameContainerWindow.innerHTML = '';
-
-  gameContainerWindow.classList.remove('small-grid','medium-grid','big-grid');
-  gameContainerWindow.classList.add(cssClass);
-
-  for (let i = 0; i < size; i += 1) {
-    const div = document.createElement('div');
-    gameContainerWindow.appendChild(div);
-    
+function createGrid(size) {
+  let grid = document.querySelector('#grid-container');
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement('div');
+    column.classList.add('column');
+    column.style.border = '2px solid black';
+    for (let j = 0; j < size; j++) {
+      let row = document.createElement('div');
+      row.classList.add('row');
+      row.style.border = '2px solid black';
+      // row.style.width = '100%';
+      row.innerText = 'CRTAJ...BOLI ME KURAC DJE';
+      column.appendChild(row);
+    }
+    grid.appendChild(column);
   }
-}
+};
 
-
-function startGame() {
-  createGrid();
-}
-
-
-startGame();
+createGrid(10);
