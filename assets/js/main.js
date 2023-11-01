5cae04efa7e2216dfaef104aed66620665c7b423
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-11-01 21:12:53 +0100

    Small Grid Button Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
-const clearBtn = document.querySelector('#clear-');
+const clearBtn = document.querySelector('#clear');
+const smallGrid = document.querySelector('#small-grid');
+const mediumGrid = document.querySelector('#medium-grid');
+let grid = document.querySelector('#grid-container');
 // const gameContainerWindow = document.querySelector('#grid-container');
 
-function createGrid(size) {
-  let grid = document.querySelector('#grid-container');
-  for (let i = 0; i < size; i++) {
+function createGrid(rows, columns) {
+  
+  grid.textContent = "";
+
+  for (let i = 0; i < columns; i++) {
     let column = document.createElement('div');
     column.classList.add('column');
     // column.style.border = '2px solid black'; // Added a double border to divs
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < rows; j++) {
       let row = document.createElement('div');
       row.classList.add('row');
       row.style.border = '2px solid black';
@@ -18,9 +23,26 @@ function createGrid(size) {
   }
 };
 
-createGrid(32);
+
+
+smallGrid.addEventListener('click', function () {
+  createGrid(16, 16);
+  row.style.height = "39.8%";
+  column.style.height = "39.8%";
+});
+
+console.log(smallGrid);
+
+
+mediumGrid.addEventListener('click', function () {
+  createGrid(32);
+  grid.style.gridTemplateColumns = "32, 1fr";
+  grid.style.gridTemplateRows = "32, 1fr";
+});
+
+console.log(mediumGrid);
 
 
 clearBtn.addEventListener('click', function () {
-  
+  grid.innerHTML = "";
 });
